f69a3cf4bebc915615c063a89bc7b33fd84a785b
Dodanim <dodanimjunior24@gmail.com>
2023-02-27 22:55:17 -0600

    WIP - Login structure

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,7 +1,15 @@
+import dodi from '../../assets/2.svg';
+import '../../styles/login.css';
 const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="body">
+      <div className="content">
+        <form action="" id="form">
+          <img src={dodi} alt="" id="logoForm" />
+          <input type="text" id="Username" />
+          <input type="password" id="Password" />
+        </form>
+      </div>
     </div>
   );
 };
