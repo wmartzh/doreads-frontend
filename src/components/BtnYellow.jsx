7874cb8906a9e351e2ea71073411e8c29c9de085
Jp121212 <jpablo1218@gmail.com>
2023-02-28 22:48:29 -0600

    DONE - Components Buttons
    Each button with its respective structure and style

---
+++ src/components/BtnYellow.jsx
@@ -0,0 +1,36 @@
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+
+function BootstrapButton2({ href, text }) {
+  const BootstrapButton2 = styled(Button)({
+    boxShadow: 'none',
+    textTransform: 'none',
+    border: 'none',
+    width: '200px',
+    height: '44px',
+    'border-radius': '10px',
+    fontSize: 16,
+    padding: '6px 12px',
+    lineHeight: 1.5,
+    margin: 'auto',
+    backgroundColor: '#E9BD1F',
+    fontFamily: ['Open Sans', 'sans-serif'].join(','),
+    '&:hover': {
+      backgroundColor: '#D4A20F',
+      boxShadow: 'none',
+      border: 'none'
+    },
+    '&:active': {
+      boxShadow: 'none',
+      backgroundColor: '#D4A20F',
+      border: 'none'
+    }
+  });
+  return (
+    <BootstrapButton2 variant="contained" disableRipple href={href}>
+      {text}
+    </BootstrapButton2>
+  );
+}
+
+export default BootstrapButton2;
