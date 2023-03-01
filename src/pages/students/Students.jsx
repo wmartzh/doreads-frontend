1e0910978fa4c33e645f1624b2b8a308c8ffe9ab
Jp121212 <jpablo1218@gmail.com>
2023-02-28 23:20:55 -0600

    DONE - Students MainFrame

--- src/pages/students/Students.jsx
+++ src/pages/students/Students.jsx
@@ -1,8 +1,22 @@
-const Students = () => {
+import Sidebar from '../../Components/SideBar';
+import Items from '../../Components/Stack2';
+import '../../styles/Students.css';
+
+const Student = () => {
   return (
-    <div>
-      <h1>Students</h1>
+    <div className="body">
+      <div className="sideBar">
+        <Sidebar />
+      </div>
+      <div className="Cont">
+        <div className="DescStudents">
+          <h1 className="Title">Description</h1>
+        </div>
+        <div className="Students">
+          <Items text1="Register Students" text2="Penalty Fee" text3="See Student" />
+        </div>
+      </div>
     </div>
   );
 };
-export default Students;
+export default Student;
