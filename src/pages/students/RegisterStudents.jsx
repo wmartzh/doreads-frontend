6fd8f2176ee93aaceec3e7faf2c12895d3e635b1
Adonis Aleman <adonis.aleman@unadeca.net>
2023-03-05 20:53:06 -0600

    Acualizacion del register se agrego el espacio para el heade y el form

--- src/pages/students/RegisterStudents.jsx
+++ src/pages/students/RegisterStudents.jsx
@@ -1,5 +1,6 @@
 import Sidebar from '../../components/SideBar';
-import registerstu from '../../assets/registerstu.svg';
+import graduated from '../../assets/graduated.svg';
+
 const RegisterStudents = () => {
   return (
     <div className="RegiserStuden">
@@ -7,42 +8,49 @@ const RegisterStudents = () => {
         <Sidebar />
       </div>
       <div className="ViewRegisterStudent">
-        <form className="form">
-          <div className="studentRegister">
-            <div>
-              <div className="PaddingStudent">
-                <h1 className="TitleRegister">Student Name</h1>
-                <div className="Input">
-                  <input type="text" id="Email" placeholder="Enter Name" />
+        <div className="HeaderStudent">
+          <h1 className="Title">Register Student</h1>
+        </div>
+        <div className="BodyStuden">
+          <div className="AlertStudent">
+            <form className="form">
+              <div className="studentRegister">
+                <div>
+                  <div className="PaddingStudent">
+                    <h1 className="TitleRegister">Student Name</h1>
+                    <div className="Input">
+                      <input type="text" id="Name" placeholder="Enter Name" required />
+                    </div>
+                  </div>
+                  <div className="PaddingStudent1">
+                    <h1 className="TitleRegister">Student Last Name</h1>
+                    <div className="Input">
+                      <input type="text" id="Last Name" placeholder="Enter Last Name" required />
+                    </div>
+                  </div>
+                  <div className="PaddingStudent2">
+                    <h1 className="TitleRegister">Student Email</h1>
+                    <div className="Input">
+                      <input type="text" id="Email" placeholder="Enter Email" required />
+                    </div>
+                  </div>
+                  <div className="PaddingStudent3">
+                    <h1 className="TitleRegister">Student Phone</h1>
+                    <div className="Input">
+                      <input type="text" id="Phone" placeholder="Enter Phone" required />
+                    </div>
+                  </div>
                 </div>
               </div>
-              <div className="PaddingStudent1">
-                <h1 className="TitleRegister">Student Last Name</h1>
-                <div className="Input">
-                  <input type="text" id="Last Name" placeholder="Enter Last Name" />
-                </div>
+              <div className="buttons">
+                <img className="ImageStudent" src={graduated} alt="register" />
+                <button className="LogiStudent" type="submit" id="RegisterButton">
+                  Register
+                </button>
               </div>
-              <div className="PaddingStudent2">
-                <h1 className="TitleRegister">Student Email</h1>
-                <div className="Input">
-                  <input type="text" id="Email" placeholder="Enter Email" />
-                </div>
-              </div>
-              <div className="PaddingStudent3">
-                <h1 className="TitleRegister">Student Phone</h1>
-                <div className="Input">
-                  <input type="text" id="Phone" placeholder="Enter Phone" />
-                </div>
-              </div>
-            </div>
+            </form>
           </div>
-          <div className="buttons">
-            <img className="LogiStudent" src={registerstu} />
-            <button className="LogiStudent" type="submit" id="RegisterButton">
-              Register
-            </button>
-          </div>
-        </form>
+        </div>
       </div>
     </div>
   );
--- src/pages/students/StudentsOverview.jsx
+++ src/pages/students/StudentsOverview.jsx
@@ -1,9 +1,18 @@
 import Sidebar from '../../components/SideBar';
+
 const StudentsOverview = () => {
   return (
     <div>
       <div className="sideBar">
         <Sidebar />
+      </div>
+      <div className="ViewStudent">
+        <div className="Header">
+          <h1 className="Title">Students Overview</h1>
+        </div>
+        <div>
+          <h1>forms</h1>
+        </div>
       </div>
     </div>
   );
