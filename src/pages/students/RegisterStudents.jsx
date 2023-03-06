6762319d2b38ea31694a81f64b08b5b6267c05a1
Adonis Aleman <adonis.aleman@unadeca.net>
2023-03-05 22:34:17 -0600

    Logica de post en axios

--- src/pages/students/RegisterStudents.jsx
+++ src/pages/students/RegisterStudents.jsx
@@ -1,7 +1,33 @@
 import Sidebar from '../../components/SideBar';
 import graduated from '../../assets/graduated.svg';
-
+import React from 'react';
+import axios from 'axios';
 const RegisterStudents = () => {
+  const [postStudent, setPostStudent] = React.useState({
+    code: '',
+    name: '',
+    email: '',
+    phone: ''
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setPostStudent((prevFormData) => ({
+      ...prevFormData,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:8000/student/register', postStudent);
+      console.log('No resivo nada' + response);
+    } catch (error) {
+      console.log('No funciono y no estou pasando por axios' + error);
+    }
+  };
+
   return (
     <div className="RegiserStuden">
       <div className="sideBar">
@@ -13,31 +39,63 @@ const RegisterStudents = () => {
         </div>
         <div className="BodyStuden">
           <div className="AlertStudent">
-            <form className="form">
+            <form onSubmit={handleSubmit} className="form">
               <div className="studentRegister">
                 <div>
                   <div className="PaddingStudent">
-                    <h1 className="TitleRegister">Student Name</h1>
+                    <h1 className="TitleRegister">Student Code</h1>
                     <div className="Input">
-                      <input type="text" id="Name" placeholder="Enter Name" required />
+                      <input
+                        type="text"
+                        name="code"
+                        value={postStudent.code}
+                        onChange={handleInputChange}
+                        id="Code"
+                        placeholder="Enter Code"
+                        required
+                      />
                     </div>
                   </div>
                   <div className="PaddingStudent1">
-                    <h1 className="TitleRegister">Student Last Name</h1>
+                    <h1 className="TitleRegister">Student Name</h1>
                     <div className="Input">
-                      <input type="text" id="Last Name" placeholder="Enter Last Name" required />
+                      <input
+                        type="text"
+                        name="name"
+                        value={postStudent.name}
+                        onChange={handleInputChange}
+                        id="Name"
+                        placeholder="Enter Name"
+                        required
+                      />
                     </div>
                   </div>
                   <div className="PaddingStudent2">
                     <h1 className="TitleRegister">Student Email</h1>
                     <div className="Input">
-                      <input type="text" id="Email" placeholder="Enter Email" required />
+                      <input
+                        type="text"
+                        name="email"
+                        value={postStudent.email}
+                        onChange={handleInputChange}
+                        id="Email"
+                        placeholder="Enter Email"
+                        required
+                      />
                     </div>
                   </div>
                   <div className="PaddingStudent3">
                     <h1 className="TitleRegister">Student Phone</h1>
                     <div className="Input">
-                      <input type="text" id="Phone" placeholder="Enter Phone" required />
+                      <input
+                        type="text"
+                        name="phone"
+                        onChange={handleInputChange}
+                        value={postStudent.phone}
+                        id="Phone"
+                        placeholder="Enter Phone"
+                        required
+                      />
                     </div>
                   </div>
                 </div>
