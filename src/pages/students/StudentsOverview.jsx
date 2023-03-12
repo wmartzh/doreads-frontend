ff877ab46173cb01fca3b84bd9b7012284a8f017
Adonis Aleman <adonis.aleman@unadeca.net>
2023-03-11 19:19:35 -0600

    Cambios de datos en la tabla de stedunt

--- src/pages/students/StudentsOverview.jsx
+++ src/pages/students/StudentsOverview.jsx
@@ -9,19 +9,19 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import TablePagination from '@mui/material/TablePagination';
 const rows = [
-  { name: 'Frozen yoghurt', calories: 159, fat: 6.0, carbs: 24, protein: 4.0 },
-  { name: 'Ice cream sandwich', calories: 237, fat: 9.0, carbs: 37, protein: 4.3 },
-  { name: 'Eclair', calories: 262, fat: 16.0, carbs: 24, protein: 6.0 },
-  { name: 'Cupcake', calories: 305, fat: 3.7, carbs: 67, protein: 4.3 },
-  { name: 'Gingerbread', calories: 356, fat: 16.0, carbs: 49, protein: 3.9 }
+  { ID: '1', FullName: 'AdonisA', Carnet: 502130458, Email: 'adoview@home.com', Status: 4.0 },
+  { ID: '2', FullName: 'AdonisA', Carnet: 502130458, Email: 'adoview@home.com', Status: 4.3 },
+  { ID: '3', FullName: 'AdonisA', Carnet: 502130458, Email: 'adoview@home.com', Status: 6.0 },
+  { ID: '4', FullName: 'AdonisA', Carnet: 502130458, Email: 'adoview@home.com', Status: 4.3 },
+  { ID: '5', FullName: 'AdonisA', Carnet: 502130458, Email: 'adoview@home.com', Status: 3.9 }
 ];
 
 const columns = [
-  { id: 'name', label: 'Dessert (100g serving)', align: 'left' },
-  { id: 'calories', label: 'Calories', align: 'right' },
-  { id: 'fat', label: 'Fat (g)', align: 'right' },
-  { id: 'carbs', label: 'Carbs (g)', align: 'right' },
-  { id: 'protein', label: 'Protein (g)', align: 'right' }
+  { id: 'ID', label: 'ID', align: 'left' },
+  { id: 'FullName', label: 'FullName', align: 'right' },
+  { id: 'Carnet', label: 'Carnet', align: 'right' },
+  { id: 'Email', label: 'Email', align: 'right' },
+  { id: 'Status', label: 'Status', align: 'right' }
 ];
 const StudentsOverview = () => {
   const [page, setPage] = React.useState(0);
