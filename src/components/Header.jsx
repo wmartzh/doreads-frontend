aef9ee92b63257a15254670b24e7401735ab9887
Jp121212 <jpablo1218@gmail.com>
2023-03-13 10:38:26 -0600

    WIP - Fix Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,20 +5,8 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
 
 export default function ButtonAppBar(props) {
-  const [anchorEl, setAnchorEl] = [null, React.useState(null)];
-
-  const handleMenu = () => {
-    setAnchorEl(false);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(true);
-  };
-
   const Header = {
     backgroundColor: '#F5F5F5',
     height: '100%',
@@ -46,27 +34,9 @@ export default function ButtonAppBar(props) {
             aria-label="account of current user"
             aria-controls="menu-appbar"
             aria-haspopup="true"
-            onClick={handleMenu}
             color="inherit">
             <Avatar />
           </IconButton>
-          <Menu
-            id="menu-appbar"
-            anchorEl={anchorEl}
-            anchorOrigin={{
-              vertical: 'top',
-              horizontal: 'right'
-            }}
-            keepMounted
-            transformOrigin={{
-              vertical: 'top',
-              horizontal: 'right'
-            }}
-            open={Boolean(anchorEl)}
-            onClose={handleClose}>
-            <MenuItem onClick={handleClose}>Profile</MenuItem>
-            <MenuItem onClick={handleClose}>My account</MenuItem>
-          </Menu>
         </Toolbar>
       </AppBar>
     </Box>
