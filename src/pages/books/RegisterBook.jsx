8d8105ba28e1829d3190788095aa0413d0e327cc
Dodanim <dodanimjunior24@gmail.com>
2023-03-24 13:48:25 -0600

    Page Structure added

--- src/pages/books/RegisterBook.jsx
+++ src/pages/books/RegisterBook.jsx
@@ -1,7 +1,18 @@
+import Form from '../../components/Form';
+
+import Header from '../../components/Header';
+// import Input from '../../components/Input';
+// import Button from '../../components/Button';
+// import createBook from '../../services/book';
 const RegisterBook = () => {
   return (
-    <div>
-      <h1>RegisterBook</h1>
+    <div className="ContMain">
+      <div className="ContHeader">
+        <Header TitlePage="Register Book"></Header>
+      </div>
+      <div className="ContForm">
+        <Form width="60%"></Form>
+      </div>
     </div>
   );
 };
