3e0849a9cd58cdee6cc24d4221400cc854b056a0
Dodanim <dodanimjunior24@gmail.com>
2023-03-26 15:32:09 -0600

    Select corrected

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -3,13 +3,18 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
+import { useState } from 'react';
 
 export default function BasicSelect(props) {
+  const [value, setValue] = useState('');
+  const handleChange = async (event) => {
+    await setValue(event.target.value);
+  };
   return (
     <Box sx={{ width: props.width }}>
       <FormControl fullWidth variant="filled">
         <InputLabel>{props.label}</InputLabel>
-        <Select>
+        <Select id={props.id} value={value} onChange={handleChange}>
           {props.options &&
             props.options.map((el, i) => {
               return (
