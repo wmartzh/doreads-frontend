ba9d5226f364713101a8d0c3215d30e31e2fbfcf
Dodanim <dodanimjunior24@gmail.com>
2023-04-03 14:02:48 -0600

    WIP - PR Corrections
    Added Model validation with JOI,
    Swapped functionality to form component instead of book service.
    Added models folder
    Corrected Alert error Handling

--- src/pages/books/RegisterBook.jsx
+++ src/pages/books/RegisterBook.jsx
@@ -6,10 +6,65 @@ import Input from '../../components/Input';
 import DatePickerComp from '../../components/DatePicker';
 import Button from '../../components/Button';
 import BasicSelect from '../../components/Select';
-import { collectBookData } from '../../services/book';
 import AlertComp from '../../components/Alert';
+import { ValidateBook } from '../../services/book';
 import { useState } from 'react';
 const RegisterBook = () => {
+  const [BookTitle, setBookTitle] = useState('');
+  const [BookAuthor, setBookAuthor] = useState('');
+  const [BookISBN, setBookISBN] = useState('');
+  const [BookCategory, setBookCategory] = useState('');
+  const [BookYear, setBookYear] = useState('');
+  const [BookPicture, setBookPicture] = useState('');
+  const [BookEditorial, setBookEditorial] = useState('');
+  const [BookLanguage, setBookLanguage] = useState('');
+  const resetForm = () => {
+    setBookTitle('');
+    setBookAuthor('');
+    setBookISBN('');
+    setBookCategory('');
+    setBookYear('');
+    setBookPicture('');
+    setBookEditorial('');
+    setBookLanguage('');
+  };
+  async function collectBookData(method) {
+    try {
+      const book = {
+        title: document.getElementById('BookTitle').value,
+        author: document.getElementById('BookAuthor').value,
+        isbn: document.getElementById('BookISBN').value,
+        category: document.getElementById('BookCategory').innerHTML.toString(),
+        year: document.getElementById('BookYear').getAttribute('value'),
+        picture: document.getElementById('BookPicture').value,
+        editorial: document.getElementById('BookEditorial').value,
+        language: document.getElementById('BookLanguage').innerHTML.toString()
+      };
+      if (ValidateBook(book)) {
+        try {
+          const ApiCall = await createBook(book);
+          console.log(ApiCall);
+          if (ApiCall.status === 200) {
+            method({ isOpen: true, message: 'Book Created', severity: 'success' });
+          } else if (ApiCall.response.status === 400) {
+            method({ isOpen: true, message: 'Book Already Exists', severity: 'error' });
+          } else {
+            method({ isOpen: true, message: 'Error Creating Book', severity: 'error' });
+          }
+        } catch (error) {
+          throw new Error('Error Creating Book');
+        }
+      } else {
+        throw new Error('Missing Fields');
+      }
+    } catch (error) {
+      if (error.message === 'Missing Fields') {
+        method({ isOpen: true, message: 'Missing Fields', severity: 'error' });
+      } else {
+        method({ isOpen: true, message: 'Error Creating Book', severity: 'error' });
+      }
+    }
+  }
   const Categoria = ['Ficcion', 'Ciencia', 'Arte', 'Tecnologia', 'Ingenieria y Matematicas'];
   const Idiomas = ['ES', 'ENG'];
   const [alert, setAlert] = useState({ isOpen: false, message: '', severity: 'success' });
@@ -25,7 +80,6 @@ const RegisterBook = () => {
             <Input
               height="44px"
               width="100%"
-              colorHover="#394c73"
               id="BookISBN"
               placeholder="Enter ISBN"
               type="text"
@@ -35,7 +89,6 @@ const RegisterBook = () => {
             <Input
               height="44px"
               width="100%"
-              colorHover="#394c73"
               id="BookTitle"
               placeholder="Enter Title"
               type="text"
@@ -45,7 +98,6 @@ const RegisterBook = () => {
             <Input
               height="44px"
               width="100%"
-              colorHover="#394c73"
               id="BookAuthor"
               placeholder="Enter Author"
               type="text"
@@ -55,7 +107,6 @@ const RegisterBook = () => {
             <Input
               height="44px"
               width="100%"
-              colorHover="#394c73"
               id="BookPicture"
               placeholder="Enter Picture URL"
               type="text"
@@ -65,7 +116,6 @@ const RegisterBook = () => {
             <Input
               height="44px"
               width="100%"
-              colorHover="#394c73"
               id="BookEditorial"
               placeholder="Enter Editorial"
               type="text"
@@ -82,7 +132,6 @@ const RegisterBook = () => {
             <Button
               TextInButton="Register Book"
               color="#394c73"
-              colorHover="#394c73"
               width="70%"
               height="44px"
               margin="15px 0px 0px 0px"
--- src/services/book.js
+++ src/services/book.js
@@ -1,51 +1,17 @@
 import axios from './axios';
+import { bookSchema } from '../models/BookModels';
 const booksBase = '/book/register';
 
 export async function createBook(book) {
   try {
     const response = await axios.post(booksBase, book);
-    return response.data;
+    return response;
   } catch (error) {
-    return [];
+    console.log(error);
+    return error.response;
   }
 }
 
-const ValidateBook = (book) => {
-  return Object.values(book).every((value) => {
-    if (value === '' || value === null || value === undefined) {
-      return false;
-    }
-    return true;
-  });
+export const ValidateBook = (book) => {
+  return bookSchema.validate(book);
 };
-
-export async function collectBookData(method) {
-  try {
-    const book = {
-      title: document.getElementById('BookTitle').value,
-      author: document.getElementById('BookAuthor').value,
-      isbn: document.getElementById('BookISBN').value,
-      category: document.getElementById('BookCategory').innerHTML.toString(),
-      year: document.getElementById('BookYear').getAttribute('value'),
-      picture: document.getElementById('BookPicture').value,
-      editorial: document.getElementById('BookEditorial').value,
-      language: document.getElementById('BookLanguage').innerHTML.toString()
-    };
-    if (ValidateBook(book)) {
-      try {
-        await createBook(book);
-        method({ isOpen: true, message: 'Book created successfully', severity: 'success' });
-      } catch (error) {
-        throw new Error('Error Creating Book');
-      }
-    } else {
-      throw new Error('Missing Fields');
-    }
-  } catch (error) {
-    if (error.message === 'Missing Fields') {
-      method({ isOpen: true, message: 'Missing Fields', severity: 'error' });
-    } else {
-      method({ isOpen: true, message: 'Error Creating Book', severity: 'error' });
-    }
-  }
-}
