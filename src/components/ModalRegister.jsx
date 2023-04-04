d3045d0df05ba8c74e389b010bf42cee70a67b5d
Adonis Aleman <adonis.aleman@unadeca.net>
2023-04-04 00:09:44 -0600

    Trabando en el modal y poder editar is not a function

--- src/components/ModalRegister.jsx
+++ src/components/ModalRegister.jsx
@@ -29,9 +29,8 @@ const style = {
   p: -3
 };
 
-export default function ModalEdit() {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
+export default function ModalEdit(props) {
+  const [open, setOpen] = useState(false);
   const handleClose = () => setOpen(false);
   const statusOptions = ['ACTIVE', 'BLOCKED', 'INACTIVE'];
 
@@ -42,6 +41,7 @@ export default function ModalEdit() {
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
   const [status, setStatus] = useState('');
+  const [students, setStudents] = useState([]);
 
   useEffect(() => {
     getStudents(0, 0, { order: 'id', sort: 'asc' }).then((students) => {
@@ -50,14 +50,16 @@ export default function ModalEdit() {
   }, []);
 
   const limit = totalStudents > 100 ? 100 : totalStudents;
-  getStudents(0, limit, { order: 'id', sort: 'asc' }).then((students) => {
-    console.log('Hola soy el get normal', students);
-  });
+  useEffect(() => {
+    getStudents(0, limit, { order: 'id', sort: 'asc' }).then((students) => {
+      setStudents(students);
+    });
+  }, [totalStudents, limit]);
 
-  const handleStudentClick = (id) => {
+  const handleOpen = (id) => {
     setSelectedStudentId(id);
+    setOpen(true);
   };
-  console.log(handleStudentClick);
 
   const handleSave = () => {
     const updatedStudent = { code, name, email, phone, status };
@@ -68,6 +70,7 @@ export default function ModalEdit() {
         .put(`http://localhost:8000/student/${selectedStudentId}`, updatedStudent)
         .then((response) => {
           console.log(response);
+          props.handleSave();
         })
         .catch((error) => {
           console.log(error);
@@ -79,85 +82,90 @@ export default function ModalEdit() {
 
   return (
     <div>
-      <BootstrapButton
-        TextIdit="Edit"
-        width="80px"
-        height="40px"
-        colorHover="#D4A20F"
-        float="left"
-        onClick={handleOpen}></BootstrapButton>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description">
-        <Box sx={style}>
-          <Form width="100%">
-            <div className="TextEdit">
-              <InputLabel InputLabel="Enter code"></InputLabel>
-              <Input
-                type="text"
-                name="code"
-                id="Code"
-                placeholder="Code: 1111-1111"
-                value={code}
-                onChange={(e) => setCode(e.target.value)}
-              />
-              <InputLabel InputLabel="Enter name"></InputLabel>
-              <Input
-                type="text"
-                name="name"
-                id="Name"
-                placeholder="Name: John Doe"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-              />
-              <InputLabel InputLabel="Enter email"></InputLabel>
-              <Input
-                type="text"
-                name="email"
-                id="Email"
-                placeholder="Email: johndoe@ejemplo.com"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <InputLabel InputLabel="Enter phone"></InputLabel>
-              <Input
-                type="text"
-                name="phone"
-                id="Phone"
-                placeholder="Phone: +506 88888888"
-                value={phone}
-                onChange={(e) => setPhone(e.target.value)}
-              />
-              <InputLabel InputLabel="Select Status"></InputLabel>
-              <Autocomplete
-                value={status}
-                onChange={(event, newValue) => {
-                  setStatus(newValue);
-                }}
-                inputValue={status}
-                onInputChange={(event, newInputValue) => {
-                  setStatus(newInputValue);
-                }}
-                id="status-autocomplete"
-                options={statusOptions}
-                sx={{ width: 530 }}
-                renderInput={(params) => <TextField {...params} label="Status" />}
-              />
-              <div className="LogiStudentt">
-                <BootstrapButton
-                  width="200px"
-                  height="44px"
-                  id="LogiStudentt"
-                  TextIdit="Save Student"
-                  onClick={handleSave}
-                />
-              </div>
-            </div>
-          </Form>
-        </Box>
-      </Modal>
+      {students.map((student) => (
+        <div key={student.id}>
+          <BootstrapButton
+            TextIdit="Edit"
+            width="80px"
+            height="40px"
+            colorHover="#D4A20F"
+            float="left"
+            onClick={() => handleOpen(student.id)}
+          />
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description">
+            <Box sx={style}>
+              <Form width="100%">
+                <div className="TextEdit">
+                  <InputLabel InputLabel="Enter code"></InputLabel>
+                  <Input
+                    type="text"
+                    name="code"
+                    id="Code"
+                    placeholder="Code: 1111-1111"
+                    value={code}
+                    onChange={(e) => setCode(e.target.value)}
+                  />
+                  <InputLabel InputLabel="Enter name"></InputLabel>
+                  <Input
+                    type="text"
+                    name="name"
+                    id="Name"
+                    placeholder="Name: John Doe"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                  <InputLabel InputLabel="Enter email"></InputLabel>
+                  <Input
+                    type="text"
+                    name="email"
+                    id="Email"
+                    placeholder="Email: johndoe@ejemplo.com"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <InputLabel InputLabel="Enter phone"></InputLabel>
+                  <Input
+                    type="text"
+                    name="phone"
+                    id="Phone"
+                    placeholder="Phone: +506 88888888"
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
+                  />
+                  <InputLabel InputLabel="Select Status"></InputLabel>
+                  <Autocomplete
+                    value={status}
+                    onChange={(event, newValue) => {
+                      setStatus(newValue);
+                    }}
+                    inputValue={status}
+                    onInputChange={(event, newInputValue) => {
+                      setStatus(newInputValue);
+                    }}
+                    id="status-autocomplete"
+                    options={statusOptions}
+                    sx={{ width: 530 }}
+                    renderInput={(params) => <TextField {...params} label="Status" />}
+                  />
+                  <div className="LogiStudentt">
+                    <BootstrapButton
+                      width="200px"
+                      height="44px"
+                      id="LogiStudentt"
+                      TextIdit="Save Student"
+                      onClick={handleSave}
+                    />
+                  </div>
+                </div>
+              </Form>
+            </Box>
+          </Modal>
+        </div>
+      ))}
     </div>
   );
 }
